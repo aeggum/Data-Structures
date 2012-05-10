5d2bac69572d9a65ce552df6b1eee8951e8d15f0
Adam Eggum <aeggum12@gmail.com>
2012-05-09 20:23:52 -0500

    Edge added

--- src/Edge.java
+++ src/Edge.java
@@ -9,4 +9,37 @@ public class Edge implements Comparable<Edge> {
 	this.src2 = from;
 	this.weight = weight;
     }
+
+    public int getWeight() {
+	return this.weight;
+    }
+
+    public int getNode() {
+	return src1;
+    }
+    
+    public int getOtherNode(int node) {
+	if (node == src1) 
+	    return src2;
+	else if (node == src2)
+	    return src1;
+	else
+	    return src2;
+    }
+
+    public int compareTo(Edge that) {
+	if (this.getWeight() < that.getWeight()) 
+	    return -1;
+	else if (this.getWeight() > that.getWeight()) 
+	    return 1;
+	else
+	    return 0;
+    }
+
+
+    public String toString() {
+	return "" + src1 + ", " + src2 + ", " + weight;
+    }
+
+    
 }
--- src/Graph2.java
+++ src/Graph2.java
@@ -13,5 +13,72 @@ public class Graph2 {
 	    adjacentEdges.add(new ArrayList<Edge>());
     }
 
+    public Graph(int numNodes, int numEdges) {
+	this(numNodes);
+	if (numEdges < 0) 
+	    throw new RuntimeException("Number of Edges must be nonnegative");
+	
+	for (int i = 0; i < numEdges; i++) {
+	    int v = (int) Math.random() * numNodes;
+	    int w = (int) Math.random() * numNodes;
+	    double weight = Math.round(100 * Math.random());
+	    Edge e = new Edge(v, w, (int) weight);
+	    addEdge(e);
+	}
+    }
+
+    public int getNumNodes() {
+	return this.numNodes;
+    }
+
+    public int numEdges() {
+	return this.numEdges;
+    }
+
+    public void addEdge(Edge edge) {
+	int node1 = edge.getNode();
+	int node2 = edge.getOtherNode(node1);
+	for (ArrayList<Edge> edgeList: adjacentEdges) {
+
+	}
+
+	adjacentEdges.get(node1).add(edge);
+	adjacentEdges.get(node2).add(edge);
+	numEdges++;
+    }
+
+
+    public ArrayList<Edge> adjacentEdges(int node) {
+	return adjacentEdges.get(node);
+    }
+
+
+    public Iterator<Edge> edges() {
+	ArrayList<Edge> list = new ArrayList<Edge>();
+	for (int i = 0; i < numNodes; i++) {
+	    for (Edge e: adjacentEdges(i)) {
+		if (e.getOtherNode(i) > i) {
+		    list.add(e);
+		}
+	    }
+	}
+
+	return list.iterator();
+    }
+
+    
+    public String toString() {
+	String NEWLINE = System.getProperty("line.separator");
+	StringBuilder s = new StringBuilder();
+	s.append(numNodes + " " + numEdges + NEWLINE);
+	for (int v = 0; v < numNodes; v++) {
+	    s.append(v + ": ");
+	    for (Edge e : adjacentEdges.get(v)) {
+		s.append(e + ", ");
+	    }
+	    s.append(NEWLINE);
+	}
+	return s.toString();
+    }
     
 }
